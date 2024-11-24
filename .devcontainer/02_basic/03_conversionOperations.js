1a0516e386ec430dccd452ead3aba07461aa535f
Jassi88 <63803662+jagroop2388@users.noreply.github.com>
2024-11-24 22:30:53 +0000

    operations methods

--- .devcontainer/02_basic/03_conversionOperations.js
+++ .devcontainer/02_basic/03_conversionOperations.js
@@ -3,11 +3,38 @@ let marks = true
 let findNumber = Number (marks)
 
 
-console.log (typeof marks);
-console.log(typeof findNumber);
-console.log(findNumber);
+// console.log (typeof marks);
+// console.log(typeof findNumber);
+// console.log(findNumber);
 
 // "560" => 560, string numbers can be converted to number
 // "560abc" >= NaN.. type is number though
 // true= 1, false = 0
+
+let loggedIn = ""
+
+let booleanIsLoggedIn = Boolean(loggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+//  "" => false
+// "jassi" true
+
+//******************Operations********************************
+
+let name1 = "jassi"
+let name2 = "singh"
+let name3 = "cheema"
+
+// console.log(name1  + " "+ name2  + " " + name3);
+// console.log ("1" + 2);  //it will concatenat the result as 12
+// console.log (1 + "2");  // it will still concatente the result as  12
+// console.log ("1" + 2 + 5); // it will also concatente the result as string as prefix is string
+// console.log (1 + 2 + "5"); //it will concatenate the result as 35, adding first sub operatio and adding last tring value
+
+let x =5
+//const y = x++
+const y = ++x
+console.log (x, y);
+
 
