bc67c3e70039e01e43734a04793607ede8a2bb2a
Jassi88 <63803662+jagroop2388@users.noreply.github.com>
2024-11-24 20:26:11 +0000

    conversionOperations

---
+++ .devcontainer/02_basic/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let marks = true
+
+let findNumber = Number (marks)
+
+
+console.log (typeof marks);
+console.log(typeof findNumber);
+console.log(findNumber);
+
+// "560" => 560, string numbers can be converted to number
+// "560abc" >= NaN.. type is number though
+// true= 1, false = 0
+
