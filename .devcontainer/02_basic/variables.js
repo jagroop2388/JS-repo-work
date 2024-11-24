562d1c21e0d8c6fbaa9806c290c07f1b620a0478
Jassi88 <63803662+jagroop2388@users.noreply.github.com>
2024-11-24 18:10:20 +0000

    variables

---
+++ .devcontainer/02_basic/variables.js
@@ -0,0 +1,10 @@
+const userId = 123456
+let userName = "jassi"
+accountPassword = "hello123"
+CurrentCity = "Amritsar"
+
+let userAge;
+
+//console.log(userId);
+
+console.table([userId, userName, accountPassword, CurrentCity, userAge]);
