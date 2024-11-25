6cdaeb76d36821d8b661e41b61765f69f300634e
Jassi88 <63803662+jagroop2388@users.noreply.github.com>
2024-11-25 01:41:23 +0000

    dataTypeSummary

---
+++ .devcontainer/02_basic/02_dataTypeSummary.js
@@ -0,0 +1,19 @@
+/*
+Primitive Data Types:
+
+Number -> Example: 42
+String -> Example: "Hello, World"
+Boolean -> Example: true or false
+null -> Example: null
+undefined -> Example: undefined
+Symbol -> Example: Symbol("unique")
+
+Non-Primitive Data Types:
+
+1.Object: Represents a collection of key-value pairs (properties and methods). Example: { name: "John", age: 30 }.
+
+2.Array: Represents a list-like collection of values, indexed by numbers (integer indices). Example: [1, 2, 3, 4].
+
+3.Function: Represents a reusable block of code that can be invoked or called with arguments. Example: function add(x, y) {
+ return x + y; }
+*/
